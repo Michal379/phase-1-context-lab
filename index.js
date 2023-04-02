8099189d87f466093285dcbe6f38a97d46fa6550
Michal <khakaimic4@gmail.com>
2023-04-02 15:31:52 +0300

    first commit

--- index.js
+++ index.js
@@ -1,23 +1,83 @@
-/* Your Code Here */
+// /* Your Code Here */
+let employeeRecord=(["firstName", "familyName", "title", payPerHour, timeInEvents[''] ,timeOutEvents['']])
+function createEmployeeRecord(employee){
+    return{ 
+        firstName:employee[0],
+         familyName:employee[1], 
+         title:employee[2],
+          payPerHour:employee[3], 
+          timeInEvents:[], 
+          timeOutEvents:[],
+    }
+}
+//createEmployeeRecords
+let employeeRecords ;
+let rows= [
+    ["moe","sizlak","barkeep", 2 ],
+    ["bartholomew","simpson","scamp", 3]
+]
+employeeRecords.length=10
+function createEmployeeRecords(employee){
+    return employee.map(record=>createEmployeeRecord(record))
+}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+ //createTimeInEvent
+ function createTimeInEvent(dateStamp){
+   let [date, hour]=dateStamp.split("")
+ this.timeInEvents.push({
+    type: "TimeIn",
+    hour: 1400,
+    date:date.split|(0,10)
+ })
+ return this
+ }
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+ //createTimeOutEvent
+ function createTimeOutEvent(dateStamp){
+    let createTimeOUtEvent;
+let [date, hour]=dateStamp.split("")
+this.timeOutEvents.push({
+   type: "TimeOut",
+   hour: 1700,
+   date:date.split(0,10)
+})
+return this
+}
 
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
+//hoursWorkedOnDate calculates that employee worked for two hours
+function hoursWorkedOnDate(employeeRecord, date){
+    const timeIn = employeeRecord.timeInEvents.find(event => event.date === date);
+    const timeOut = employeeRecord.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour)/100;
+  }
+
+//wagesEarnedOnDate
+function wagesEarnedOnDate(employeeRecord, date){
+    return hoursWorkedOnDate(employeeRecord,date)*employeeRecord.payPerHour;
+}
+
+//allWagesFor
+function allWagesFor() {
+    let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
 
-    const payable = eligibleDates.reduce(function (memo, d) {
+    let payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+//findEmployeeByFirstName
+function findEmployeeByFirstName(source,name){
+    return source.find(record=>record.firstName===name)
+ }
+
+//calculatePayroll
+ function  calculatePayroll(employeeRecords){
+    return employeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
 }
 
